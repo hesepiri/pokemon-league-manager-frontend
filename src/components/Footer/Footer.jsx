295f4d72d6fdd9866b67c4659503730539d6d21c
hesepiri <hesepiri@gmail.com>
2026-06-04 02:10:59 -0600

    style: fix form elements font inheritance and finalize landing responsive layout

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,40 +1,74 @@
 import React from "react";
 import "./Footer.css";
 
+// Importamos tus archivos SVG desde tu carpeta de imágenes
+import githubIcon from "../../images/github-logo-svgrepo-com.svg";
+import linkedinIcon from "../../images/linkedin-logo-svgrepo-com.svg";
+import mailIcon from "../../images/mail-svgrepo-com.svg";
+
 function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
     <footer className="footer">
       <div className="footer__container">
+        {/* Lado izquierdo: Derechos reservados */}
         <p className="footer__copyright">
-          &copy; {currentYear} Pokemon League Manager. Desarrollado por Héctor
+          © {currentYear} Pokémon League Manager. Desarrollado por Héctor
           Pinedo.
         </p>
-        <nav className="footer__navigation">
-          <ul className="footer__links">
-            <li className="footer__item">
+
+        {/* Lado derecho: Enlaces y redes sociales en un solo bloque */}
+        <div className="footer__navigation">
+          <nav>
+            <a
+              href="https://tripleten.mx/"
+              className="footer__link-text"
+              target="_blank"
+              rel="noreferrer"
+            >
+              TripleTen
+            </a>
+          </nav>
+
+          <ul className="footer__social-icons">
+            <li className="footer__social-item">
               <a
                 href="https://github.com/hesepiri"
-                className="footer__link"
+                className="footer__social-link"
                 target="_blank"
                 rel="noreferrer"
+                title="GitHub"
               >
-                GitHub
+                <img src={githubIcon} alt="GitHub" className="footer__icon" />
               </a>
             </li>
-            <li className="footer__item">
+            <li className="footer__social-item">
               <a
-                href="https://tripleten.com/es-mex/?from=us"
-                className="footer__link"
+                href="https://www.linkedin.com/in/hectors-pinedorivera"
+                className="footer__social-link"
                 target="_blank"
                 rel="noreferrer"
+                title="LinkedIn"
               >
-                TripleTen
+                <img
+                  src={linkedinIcon}
+                  alt="LinkedIn"
+                  className="footer__icon"
+                />
+              </a>
+            </li>
+            <li className="footer__social-item">
+              <a
+                href="mailto:hector.pinedorivera@gmail.com"
+                className="footer__social-link"
+                title="Correo Electrónico"
+              >
+                <img src={mailIcon} alt="Contacto" className="footer__icon" />
               </a>
             </li>
           </ul>
-        </nav>
+        </div>
       </div>
     </footer>
   );
